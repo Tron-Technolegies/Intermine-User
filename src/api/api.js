aee561d6f19390c4109607bc59ed0b1fb79cc7c1
clawlynx <clawlynx77@gmail.com>
2025-12-20 13:34:56 +0530

    added final production

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://intermine-backend.onrender.com/api/v1/",
+  baseURL: "https://api.intermine-solutions.de/api/v1/",
   withCredentials: true,
 });
 
