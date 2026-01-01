d2e4bccf07a5d4c4fd50cb96eabf6020c9aa7d75
clawlynx <clawlynx77@gmail.com>
2026-01-01 07:48:36 +0530

    added new url

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1/",
+  baseURL: "https://api.intermine-solutions.de/api/v1/",
   withCredentials: true,
 });
 
 export default api;
 //https://api.intermine-solutions.de/api/v1/
+// http://localhost:3000/api/v1/
